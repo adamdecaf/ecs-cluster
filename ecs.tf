36c7a46d8f9c7d7202277ac1013dd66b2a9aab44
Adam Shannon <adamkshannon@gmail.com>
2016-08-17 17:48:45 -0500

    provide the ecs cluster id as an output

--- ecs.tf
+++ ecs.tf
@@ -112,3 +112,7 @@ resource "aws_autoscaling_group" "ecs" {
 resource "aws_ecs_cluster" "production" {
   name = "${var.ecs_cluster_name}"
 }
+
+output "ecs_cluster_id" {
+  value = "${aws_ecs_cluster.production.id}"
+}
